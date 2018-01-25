37a96d42da09faf532c0bbc9466051d5f116b344
Andrew Scott <andj.scott@gmail.com>
2018-01-25 22:50:17 +0000

    Support currencies

--- beancount/prices/sources/financequote.pl
+++ beancount/prices/sources/financequote.pl
@@ -5,12 +5,34 @@ use warnings;
 
 use Finance::Quote;
 use JSON;
+use POSIX qw(strftime);
 
 my $module = $ARGV[0];
 my $isin = $ARGV[1];
 
+
+my %grades;
+$grades{"Foo Bar"}{Mathematics}   = 97;
+$grades{"Foo Bar"}{Literature}    = 67;
+$grades{"Peti Bar"}{Literature}   = 88;
+$grades{"Peti Bar"}{Mathematics}  = 82;
+$grades{"Peti Bar"}{Art}          = 99;
+
+
+use Data::Dumper qw(Dumper);
+
 my $q = Finance::Quote->new;
+my %info = ();
 
-my %info = $q->fetch($module, $isin);
-
+if ($module eq "CURRENCY") {
+    my $symbol_a = substr($isin, 0, 3);
+    my $symbol_b = substr($isin, -3,3);
+    %info = (
+	"$isin-price"    => $q->currency($symbol_a, $symbol_b),
+	"$isin-date"     => strftime("%m/%d/%Y", localtime),
+        "$isin-currency" => $symbol_b,
+	)
+} else {
+    %info = $q->fetch($module, $isin);
+}
 print encode_json \%info;
